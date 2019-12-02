d8021c55ed896042699541d9148c96dc5626ec08
MaxKurii <kuriimaxym@gmail.com>
2019-12-02 16:12:16 +0300

    add search form

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,51 @@
-import React from 'react';
-import './App.css';
-import Header from './components/header/header';
-import CategoriesFilterComponent from './components/category/categories';
+import React from "react";
+import "./App.css";
+import Header from "./components/header/header";
+import CategoriesFilterComponent from "./components/category/categories";
+import MySearch from "./components/search/search";
 
 let isLoggedIn = false;
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <MySearch />
       {/* <SearchForm/> */}
+      <div>{isAuthenticated() ? <Logout /> : <Login />}</div>
       <div>
-      {isAuthenticated() ? <Logout/> : <Login/>}</div>
-    <div>
-      <CategoriesFilterComponent/>
-    </div>
+        <CategoriesFilterComponent />
+      </div>
     </div>
   );
 }
 
-function isAuthenticated(){
+function isAuthenticated() {
   return isLoggedIn;
 }
 
-function Logout(){
-  return (<button onClick={()=>{isLoggedIn=!isLoggedIn}}>Logout</button>)
+function Logout() {
+  return (
+    <button
+      onClick={() => {
+        isLoggedIn = !isLoggedIn;
+      }}
+    >
+      Logout
+    </button>
+  );
 }
 
-function Login(){
-  return (<button onClick={()=>{isLoggedIn=!isLoggedIn}}>Login</button>)
+function Login() {
+  return (
+    <button
+      onClick={() => {
+        isLoggedIn = !isLoggedIn;
+      }}
+    >
+      Login
+    </button>
+  );
 }
 
 export default App;
---
+++ src/components/search/search.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MySearch() {
+    return (
+        <form className="example">
+            <input type="text" placeholder="Search.." name="search"></input>
+            <button type="submit">Искать</button>
+        </form>
+
+    )
+}
+
+export default MySearch;
