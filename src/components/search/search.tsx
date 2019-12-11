9ecfa94b42c5a9e9faea2f34b61092df991f531f
MaxKurii <kuriimaxym@gmail.com>
2019-12-11 17:36:04 +0300

    addLogic

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import styles from './search.module.css'
 
-function MySearch() {
+function MySearch(props) {
     return (
-        <form className="example">
-            <input type="text" placeholder="Search.." name="search"></input>
-            <button type="submit">Искать</button>
+        <form className="example" onSubmit={props.onSearchSubmited}>
+            <input className={styles.mySearch} type="text" placeholder="Search.." name="search"
+                onChange={props.onSearchFieldChanged}></input>
+            <select onChange={props.onSelected}>
+                <option value='name'>Имя</option>
+                <option value='price'>Цена</option>
+
+            </select>
+
+            <button className={styles.mySearch} type="submit">Искать ...</button>
         </form>
 
     )
