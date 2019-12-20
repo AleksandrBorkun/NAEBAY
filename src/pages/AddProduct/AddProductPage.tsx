d13c52f241fc582ae1c2a9eceacd5cc28543292e
Aleksandr Borkun <albo@trustpilot.com>
2019-12-20 13:45:47 +0100

    remove unused div

--- src/pages/AddProduct/AddProductPage.tsx
+++ src/pages/AddProduct/AddProductPage.tsx
@@ -6,78 +6,76 @@ import FormFieldsSet from '../../components/forms/FormFieldsSet';
 
 export default function (props) {
     return (
-        <div>
+        <form className='Add New Product' id='title' onSubmit={props.onProductAddSubmited}>
             <h1 style={styles}>Add New Product!</h1>
-            <form className='Add New Product' id='title' onSubmit={props.onProductAddSubmited}>
-                <FormFieldsSet
-                    legend='Product Details'
-                    formFields={ // < - - - - cюда впихиваем все элементы которые должны быть в сете через запятую
-                        [
-                            <FormInputField
-                                label='Title*'
-                                type='text'
-                                onChange={props.onFormFieldChanged}
-                                name='name'
-                                placeholder='Set Title Here' />,
-                            <FormSelectField
-                                label='Categories*'
-                                onChange={props.onFormFieldChanged}
-                                name='categories'
-                                values={props.categories} />,
-                            <FormInputField
-                                label='Description*'
-                                type='text'
-                                onChange={props.onFormFieldChanged}
-                                placeholder='Set Title Here'
-                                name='desc' />,
-                            <FormInputField
-                                label='Price *'
-                                type='text'
-                                placeholder='set price'
-                                name='price'
-                                onChange={props.onFormFieldChanged} />,
-                            <FormInputField
-                                label='Image *'
-                                type='file'
-                                onChange={props.handleFilesChosen}
-                                name='image'
-                                multiple={true}/>,
-                            <AddedFilesList {...props} />
-                        ]
-                    } 
-                />
-                <FormFieldsSet
-                    legend='Contac Information'
-                    formFields={[
+            <FormFieldsSet
+                legend='Product Details'
+                formFields={ // < - - - - cюда впихиваем все элементы которые должны быть в сете через запятую
+                    [
                         <FormInputField
-                            label='Address *'
-                            type="text"
-                            placeholder="Set Address"
-                            name='Address'
-                            onChange={props.onFormFieldChanged}/>,
-                        <FormInputField
-                            label='Phone Number'
+                            label='Title*'
                             type='text'
-                            placeholder="Phone number"
-                            name='Phone number'
-                            onChange={props.onFormFieldChanged}/>,
-                        <FormInputField
-                            label='The contact person'
-                            type='text'
-                            placeholder="The contact person"
-                            name='The contact person'
-                            onChange={props.onFormFieldChanged}/>,
-                        <FormSelectField
-                            label='Payment Type'
                             onChange={props.onFormFieldChanged}
-                            name='paytype'
-                            values={['PayPal', 'Cash', 'Bank']} />
-                        ]}
-                    />
-                <button type='submit'>Revise</button>
-                <button type='submit'>Save</button>
-            </form>
-        </div>
+                            name='name'
+                            placeholder='Set Title Here' />,
+                        <FormSelectField
+                            label='Categories*'
+                            onChange={props.onFormFieldChanged}
+                            name='categories'
+                            values={props.categories} />,
+                        <FormInputField
+                            label='Description*'
+                            type='text'
+                            onChange={props.onFormFieldChanged}
+                            placeholder='Set Title Here'
+                            name='desc' />,
+                        <FormInputField
+                            label='Price *'
+                            type='text'
+                            placeholder='set price'
+                            name='price'
+                            onChange={props.onFormFieldChanged} />,
+                        <FormInputField
+                            label='Image *'
+                            type='file'
+                            onChange={props.handleFilesChosen}
+                            name='image'
+                            multiple={true} />,
+                        <AddedFilesList {...props} />
+                    ]
+                }
+            />
+            <FormFieldsSet
+                legend='Contac Information'
+                formFields={[
+                    <FormInputField
+                        label='Address *'
+                        type="text"
+                        placeholder="Set Address"
+                        name='Address'
+                        onChange={props.onFormFieldChanged} />,
+                    <FormInputField
+                        label='Phone Number'
+                        type='text'
+                        placeholder="Phone number"
+                        name='Phone number'
+                        onChange={props.onFormFieldChanged} />,
+                    <FormInputField
+                        label='The contact person'
+                        type='text'
+                        placeholder="The contact person"
+                        name='The contact person'
+                        onChange={props.onFormFieldChanged} />,
+                    <FormSelectField
+                        label='Payment Type'
+                        onChange={props.onFormFieldChanged}
+                        name='paytype'
+                        values={['PayPal', 'Cash', 'Bank']} />
+                ]}
+            />
+            <button type='submit'>Revise</button>
+            <button type='submit'>Save</button>
+        </form>
     )
 }
 
@@ -85,7 +83,7 @@ function AddedFilesList(props) {
     return <ul>
         {props.addedFiles.map((image: File, key) => {
             return (
-                <li key={key}> 
+                <li key={key}>
                     {image.name} - {image.size}Kb
                     <button onClick={() => props.handleRemoveClicked(image)}>X</button>
                 </li>
